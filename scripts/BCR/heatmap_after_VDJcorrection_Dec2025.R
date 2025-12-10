e3430e0c6e8e95d12707861570007ef9ea82f238
AnnaNamLab <annanamlab@gmail.com>
2025-12-10 11:49:22 -0500

    Update heavy chain file path in heatmap script

--- scripts/BCR/heatmap_after_VDJcorrection_Dec2025.R
+++ scripts/BCR/heatmap_after_VDJcorrection_Dec2025.R
@@ -19,7 +19,7 @@ DominantChain = "IGL"
 metadata_file = "/Users/saramoein/Documents/new_run_HL_May2025/2025-07-03_CellMetadata_HL1-24incHL8R_RetainedCellsOnly_MainCellTypeAndSubtypeNames.csv"
 input_file = paste0(sample,'_FILTERED_out_clone_dominantChain_',DominantChain,'.csv')
 light_chain_cellStatus = Sys.glob(paste0('/Users/saramoein/Documents/new_run_HL_May2025/FINAL_doublets_BCR_thre07_ent08/',sample,'_Rawdata_IGK_IGL.csv'))
-heavy_chain_cellStatus = Sys.glob(paste0('/Users/saramoein/Documents/new_run_HL_May2025/FINAL_doublets_BCR_thre07_ent08/',sample,'_Rawdata_IGHL.csv'))
+heavy_chain_cellStatus = Sys.glob(paste0('/Users/saramoein/Documents/new_run_HL_May2025/FINAL_doublets_BCR_thre07_ent08/',sample,'_Rawdata_IGH.csv'))
 output_directory = paste0('/Users/saramoein/Documents/new_run_HL_May2025/',sample)
 
 
