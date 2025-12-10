1c78b90eceb78a3a6d07a22b14c1324be90aa8da
AnnaNamLab <annanamlab@gmail.com>
2025-12-10 16:03:23 -0500

    Refactor IgBLAST script for chain-specific processing

--- tools/run_igblast/igblast_preprocess.sh
+++ tools/run_igblast/igblast_preprocess.sh
@@ -30,7 +30,7 @@ cd "$working_dir" || {
 # -----------------------------
 # Locate *_annot.fa file
 # -----------------------------
-annot_fa=$(ls singleline*_annot.fa 2>/dev/null | head -n 1)
+annot_fa=$(ls singleline*_annot.fa 2>/dev/null | head -n 1)  ### trust4 annot.fa file
 
 if [[ -z "$annot_fa" ]]; then
   echo "Error: No *_annot.fa file found in $working_dir"
@@ -58,17 +58,57 @@ grep -w -A 1 -f "$contig_file" fix_my_sample.fa --no-group-separator > sub_fix_m
 # -----------------------------
 # Run IgBLAST
 # -----------------------------
-/athena/namlab/scratch/sam4032/immcantation-master/scripts/ncbi-igblast-1.17.0/bin/igblastn \
-  -germline_db_V /home/sam4032/share/igblast/database/imgt_human_IGHV_clean.fasta \
-  -num_alignments_V 3 \
-  -germline_db_D /home/sam4032/share/igblast/database/imgt_human_IGHD_clean.fasta \
-  -num_alignments_D 3 \
-  -germline_db_J /home/sam4032/share/igblast/database/imgt_human_IGHJ_clean.fasta \
-  -num_alignments_J 3 \
-  -query sub_fix_my_sample_${chain}.fa \
-  -organism human \
-  -outfmt 19 \
-  -out ${sample}_igblast_output2_${chain}.txt
+
+
+if [[ "$chain" == "IGL" ]]; then
+    echo "Running IgBlast for IGL chain..."
+    igblastn \
+        -germline_db_V /home/sam4032/share/igblast/database/imgt_human_IGLV_clean.fasta \
+        -num_alignments_V 3 \
+        -germline_db_D /home/sam4032/share/igblast/database/empty_IGLD.fasta \
+        -num_alignments_D 3 \
+        -germline_db_J /home/sam4032/share/igblast/database/imgt_human_IGLJ_clean.fasta \
+        -num_alignments_J 3 \
+        -query sub_fix_my_sample_${chain}.fa \
+        -organism human \
+        -outfmt 19 \
+        -out ${sample}_igblast_output2_${chain}.txt
+
+elif [[ "$chain" == "IGK" ]]; then
+    echo "Running IgBlast for IGK chain..."
+    igblastn \
+        -germline_db_V /home/sam4032/share/igblast/database/imgt_human_IGKV_clean.fasta \
+        -num_alignments_V 3 \
+        -germline_db_D /home/sam4032/share/igblast/database/empty_IGKD.fasta \
+        -num_alignments_D 3 \
+        -germline_db_J /home/sam4032/share/igblast/database/imgt_human_IGKJ_clean.fasta \
+        -num_alignments_J 3 \
+        -query sub_fix_my_sample_${chain}.fa \
+        -organism human \
+        -outfmt 19 \
+        -out ${sample}_igblast_output2_${chain}.txt
+
+elif [[ "$chain" == "IGH" ]]; then
+    echo "Running IgBlast for IGH chain..."
+    igblastn \
+        -germline_db_V /home/sam4032/share/igblast/database/imgt_human_IGHV_clean.fasta \
+        -num_alignments_V 3 \
+        -germline_db_D /home/sam4032/share/igblast/database/imgt_human_IGHD_clean.fasta \
+        -num_alignments_D 3 \
+        -germline_db_J /home/sam4032/share/igblast/database/imgt_human_IGHJ_clean.fasta \
+        -num_alignments_J 3 \
+        -query sub_fix_my_sample_${chain}.fa \
+        -organism human \
+        -outfmt 19 \
+        -out ${sample}_igblast_output2_${chain}_test1.txt
+
+else
+    echo "ERROR: Unsupported chain '$chain'. Must be IGH, IGK, or IGL."
+    exit 1
+fi
+
+
+
 
 echo "IgBLAST completed: ${sample}_igblast_output2_${chain}.txt"
 
