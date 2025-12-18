eacf93c2d949cf158f5bda23a13e5035ed05e38e
AnnaNamLab <annanamlab@gmail.com>
2025-12-18 10:43:50 -0500

    Update HRS_contigs.R
    Signed-off-by: AnnaNamLab <annanamlab@gmail.com>

--- scripts/BCR/HRS_contigs.R
+++ scripts/BCR/HRS_contigs.R
@@ -1,20 +1,17 @@
-### this code has 2 main steps for fixing V/D/J assignment obtained from TRUST4. Step 1 is based on igblast, and step2 is based on fasta file to find any contig that has similar V/D/J as 
-### dominant V/D/J (clonal V/D/J).
+
 
 library(dplyr)
-#########STEP1#######
 
-### step1 receives all the HRS_contigs of a sample and generates the igblast VDJs from trust4 fasta file and then merges the generated igblast V/D/Js as new columns 
-### to the trust4_clustered file. Then Any of the contigs that their igblast V/D/J is the same as dominant VDJ, then it is replaced. Else, the trust4 V/D/J is returened.
-### input dominant V_D_J ; this is obtained from the heatmap in previous steps
+
+# this step is for generating the HRS_contigs required for IgBlast run
 
 sample="sample1"
-setwd(paste0('/Users/saramoein/Documents/new_run_HL_May2025/',sample))
-FILTERED_out_clone=read.table('cluster_clone.tsv',sep='\t')
+setwd(paste0('set_working_directory/',sample))
+FILTERED_out_clone=read.table('cluster_clone.tsv',sep='\t') # output from trust-cluster.py
 
 ## extracting the dominant clone
 View(FILTERED_out_clone) # 
-dominant_V="IGLV4-69*01"
+dominant_V="IGLV4-69*01" 
 dominant_D="*"
 dominant_J="IGLJ1*01"
 chain=substr(dominant_V,1,3)
@@ -32,14 +29,14 @@ FILTERED_out_clone$chain=substr(FILTERED_out_clone$V_gene,1,3)
 FILTERED_out_clone_dominantChain=FILTERED_out_clone[which(FILTERED_out_clone$chain==chain),]
 
 
-### the cuurent VDJ gene columns are renamed to TRUST4_V/D/J_calles
+## the cuurent VDJ gene columns are renamed to TRUST4_V/D/J_calles
 colnames(FILTERED_out_clone_dominantChain)[3]<-"trust4_v_call"
 colnames(FILTERED_out_clone_dominantChain)[4]<-"trust4_d_call"
 colnames(FILTERED_out_clone_dominantChain)[5]<-"trust4_j_call"
 colnames(FILTERED_out_clone_dominantChain)[14]<-"sequence_id"
 
 
-### extracting the HRS contigs from data 
+## extracting the HRS contigs from data 
 FILTERED_out_clone_dominantChain$cell_id1= substr(FILTERED_out_clone_dominantChain$sequence_id,1,16)
 FILTERED_out_clone_dominantChain=FILTERED_out_clone_dominantChain[FILTERED_out_clone_dominantChain$cell_id1 %in% sample_cell_type_annotation$cell_id1,]
 FILTERED_out_clone_dominantChain_index= match(FILTERED_out_clone_dominantChain$cell_id1, sample_cell_type_annotation$cell_id1)
@@ -55,6 +52,6 @@ singlet= unique(pilot_cellStatus_file$cell_id1[pilot_cellStatus_file$final_ReadS
 
 HRS_FILTERED_out_clone_dominantChain= HRS_FILTERED_out_clone_dominantChain[HRS_FILTERED_out_clone_dominantChain$cell_id1 %in% (singlet),]
 
-### HRS contigs are used for the next step for igblasting
+## HRS contigs are used for the next step for igblasting
 write.table(HRS_FILTERED_out_clone_dominantChain$sequence_id,paste0('contigs_',sample,'_',chain,'.txt'),row.names= FALSE, col.names= FALSE, quote= FALSE)
 
